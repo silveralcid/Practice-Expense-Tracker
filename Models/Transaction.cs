d1274bdffe7697209792ed51509a1184f34d5318
silveralcid <silveralcid@gmail.com>
2024-05-26 08:47:49 -0400

    Hooked up actions for transaction for submissions.

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -10,13 +10,13 @@ namespace Expense_Tracker.Models
 
         //Category Id
         public int CategoryId {  get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
 
         public int Amount { get; set; }
 
         [Column(TypeName = "nvarchar(75)")]
         public string? Note { get; set; }
-
+    
         public DateTime Date {  get; set; } = DateTime.Now;
     }
 }
