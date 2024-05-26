372c47c74e5a00ec2ecf41c52d7d66c16af1eeaf
silveralcid <silveralcid@gmail.com>
2024-05-26 09:11:05 -0400

    Transaction index grid setup, fixed syntax [1:34:17]

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Expense_Tracker.Models
@@ -9,14 +10,24 @@ namespace Expense_Tracker.Models
         public int TransactionId { get; set; }
 
         //Category Id
-        public int CategoryId {  get; set; }
+        public int CategoryId { get; set; }
         public Category? Category { get; set; }
 
         public int Amount { get; set; }
 
         [Column(TypeName = "nvarchar(75)")]
         public string? Note { get; set; }
-    
-        public DateTime Date {  get; set; } = DateTime.Now;
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleWithIcon
+        {
+            get
+            {
+                return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+        }
+            
     }
 }
